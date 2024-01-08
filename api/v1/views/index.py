27475b64449837cac71775031ae895110cbd6c13
HanRachid <handaouirachid@gmail.com>
2024-01-08 19:38:05 +0100

    add stats endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -10,6 +11,20 @@ def status():
     """returns status
         """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stat_count():
+    """ endpoint that retrieves the # of each objects by type """
+    count_stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(count_stats)
 
 
 if __name__ == "__main__":
