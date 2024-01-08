763ecf2bfd569dea7e8d2237b677c0d05e638c7d
HanRachid <handaouirachid@gmail.com>
2024-01-08 20:30:23 +0100

    fix bug

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """starting point of our app
 """
-from flask import Flask, make_response, jsonify
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
-from flask_cors import CORS
 import os
 
 app = Flask(__name__)
@@ -18,11 +17,6 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
-
-
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 if __name__ == '__main__':
